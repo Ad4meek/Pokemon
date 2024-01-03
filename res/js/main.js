0dca8ab4ce65c4e55bce97ffe46cada5320aee50
adam <adam.erben@seznam.cz>
2024-01-03 07:19:55 +0100

    map, images

---
+++ res/js/main.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d')
+
+canvas.width = 1280
+canvas.height = 640
+
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = '../res/img/maps/testmap.png'
+
+image.onload = () => {
+    ctx.drawImage(image, -1900, -600)
+}
+
+const character = new Pharacter()
+character.src = '../res/img/characters/characterDown.png'
+
+character.onload = () => {
+    ctx.drawImage(character, -1900, -600)
+}
