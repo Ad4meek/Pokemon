3c2e892c1c9f3086a4bf16ad17041529c060a09b
adam <adam.erben@seznam.cz>
2024-01-03 22:06:12 +0100

    small change

--- res/js/main.js
+++ res/js/main.js
@@ -7,14 +7,14 @@ canvas.height = 640
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
-image.src = '../res/img/maps/testmap.png'
+image.src = './res/img/maps/testmap.png'
 
 image.onload = () => {
     ctx.drawImage(image, -1900, -600)
 }
 
 const character = new Image()
-character.src = '../res/img/characters/characterDown.png'
+character.src = './res/img/characters/characterDown.png'
 
 character.onload = () => {
     ctx.drawImage(character, -1900, -600)
