7ffb1b4ed31ccb00cc0d2b8c8f2ef0ee9dcdde76
adam <adam.erben@seznam.cz>
2024-02-07 17:50:37 +0100

    canvas resize

--- res/js/main.js
+++ res/js/main.js
@@ -3,8 +3,8 @@ import { map } from "./map.js";
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1280;
-canvas.height = 640;
+canvas.width = 1600;
+canvas.height = 800;
 
 // Collisions
 
